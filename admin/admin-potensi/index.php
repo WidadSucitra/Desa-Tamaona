a1a3dfe28c6ba58262035eee398364ead281d315
Andi WIdad Sucitra <sucitraandiwidad@gmail.com>
2022-05-24 23:14:32 +0800

    crud halaman potensi

--- admin/admin-potensi/index.php
+++ admin/admin-potensi/index.php
@@ -19,9 +19,51 @@ include "../includes/navbar.php";
 
         <!-- Begin Page Content -->
         <div class="container-fluid">
+          <section class="index-potensi">
+            <!-- Page Heading -->
+            <h1 class="h3 mb-4 text-gray-800">Potensi Desa Tamaona</h1>
+            <a href="create.php"><button class="btn-index-potensi">Tambah Potensi</button></a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Jenis Potensi</th>
+                  <th scope="col">Keterangan</th>
+                  <th scope="col">Gambar</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php
+                  $query = "SELECT * FROM potensi_desa ORDER BY id ASC";
+                  $result = mysqli_query($conn, $query);
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                  if(!$result) {
+                    die("Query Error : ".mysqli_errno($conn)." - ".mysqli_errno($conn));
+                  }
+                  $no = 1;
+
+                  while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                <tr>
+                  <td><?php echo $no; ?></td>
+                  <td><?php echo $row['jenis_potensi']; ?></td>
+                  <td><?php echo substr($row['ket'], 0, 1000); ?></td>
+                  <td><img src="../uploads/jenis_potensi/<?php echo $row['image_url']; ?>"></td>
+                  <td>
+                    <a href="edit.php?id=<?= $row['id'] ?>"><button class="btn btn-warning">Edit</button></a>
+                    <a href="hapus.php<?php echo $row ?>"><button class="btn btn-danger">Hapus</button></a>
+                  </td>
+                </tr>
+                <?php
+                  $no++;
+                }
+                ?>
+              </tbody>
+              </table>
+
+          </section>
+
 
         </div>
         <!-- /.container-fluid -->
