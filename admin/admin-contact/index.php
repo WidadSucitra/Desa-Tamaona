1a27518da6c59794d4794b3fe836964edb626437
AndiBesseAdya <adyaadias87@gmail.com>
2022-05-26 17:18:57 +0800

    crud halaman kontak

--- admin/admin-contact/index.php
+++ admin/admin-contact/index.php
@@ -1,15 +1,93 @@
 <?php
+ob_start(); 
 include "../config.php";
 include "../includes/header.php"; 
 include "../includes/navbar.php"; 
+
+if(isset($_POST['submit_delete'])){
+  $contact_id = $_POST['submit_delete'];
+
+  $query = "DELETE FROM hubungi_kami WHERE id='$contact_id' LIMIT 1";
+  $query_run = mysqli_query($conn,$query);
+
+  if($query_run)
+    {
+      // $_SESSION['message'] = "Data berhasil ditambahkan!";
+      header('Location: index.php');
+      exit(0);
+  }
+  else
+  {
+    $_SESSION['message'] = "Something went wrong";
+    header('Location: index.php');
+      exit(0);
+    }
+}
 
 ?>
 
         <!-- Begin Page Content -->
         <div class="container-fluid">
+          <section class="index-event">
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+            <!-- Page Heading -->
+            <h1 class="h3 mb-4 text-gray-800">Daftar Contact Us</h1>
+
+            <!-- <button class="btn-index-event"><a href="create.php">Tambah Event</a></button> -->
+
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">No.</th>
+                  <th scope="col">Nama</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Perusahaan</th>
+                  <th scope="col">Telepone</th>
+                  <th scope="col">Pesan</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  $query = "SELECT * FROM hubungi_kami ORDER BY id ASC";
+                  $result = mysqli_query($conn, $query);
+
+                  if(!$result) {
+                    die("Query Error : ".mysqli_errno($conn)." - ".mysqli_errno($conn));
+                  }
+                  $no = 1;
+
+                  while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                <tr>
+                  <td><?php echo $no; ?></td>
+                  <td><?php echo $row['nama']; ?></td>
+                  <td><?php echo $row['email']; ?></td>
+                  <td><?php echo $row['perusahaan']; ?></td>
+                  <td><?php echo $row['telepone']; ?></td>
+                  <td width=50%><?php echo substr($row['pesan'], 0, 1000); ?>...</td>
+                  
+                  <td class="btn-index">
+                  <!-- <a href="edit.php?id=<?= $row['id'] ?>"><button class="btn btn-warning">Edit</button></a>
+                    <a href="proses_hapus.php<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin ingin hapus data ini?')"><button class="btn btn-danger">Hapus</button></a> -->
+                    
+                    <form method="POST">
+                      <button type="submit" name="submit_delete" value="<?= $row['id'] ?>" onclick="return confirm('Anda yakin ingin hapus data ini?')" class="btn btn-danger">Hapus</button>
+                    </form>
+                  </td>
+                </tr>
+                <?php
+                  $no++;
+                }
+                ?>
+
+              </tbody>
+              </table>
+
+           
+
+
+          </section>
 
         </div>
         <!-- /.container-fluid -->
--- contact.php
+++ contact.php
@@ -1,6 +1,33 @@
 <?php 
+ob_start();
 include "navbar.php"; 
 include "admin/config.php";
+
+if (isset($_POST['submit'])) {
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $perusahaan = $_POST['perusahaan'];
+    $telepone = $_POST['telepone'];
+    $pesan = $_POST['pesan'];
+  
+    $query = "INSERT INTO hubungi_kami (nama,email,perusahaan,telepone,pesan) 
+                          VALUES('$nama','$email','$perusahaan','$telepone','$pesan')";
+  
+      $query_run=mysqli_query($conn, $query);
+  
+    if($query_run)
+    {
+        header('Location: index.php');
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Something went wrong";
+        header('Location: index.php');
+        exit(0);
+    }
+  }
+
 ?>
   <!-- jumbotron -->
   <section class="d-flex align-items-center">
@@ -23,14 +50,14 @@ include "admin/config.php";
               </div>
               <div class="right">
                   <div class="right-container">
-                      <form action="">
+                      <form method="post" enctype="multipart/form-data">
                           <h2>Hubungi Kami</h2>
-                          <input type="text" placeholder="Nama">
-                          <input type="email" placeholder="Alamat Email">
-                          <input type="text" placeholder="Perusahaan">
-                          <input type="phone" placeholder="Telepone">
-                          <textarea rows="10" placeholder="Pesan"></textarea>
-                          <button>Kirim</button>
+                          <input type="text" id="nama" name="nama" placeholder="Nama">
+                          <input type="email" id="email" name="email" placeholder="Alamat Email">
+                          <input type="text" id="perusahaan" name="perusahaan" placeholder="Perusahaan">
+                          <input type="text" id="telepone" name="telepone" placeholder="Telepone">
+                          <textarea rows="10" id="pesan" name="pesan" placeholder="Pesan"></textarea>
+                          <button type="submit" name="submit" value="Upload"><a href="contact.php">Kirim</a></button>
                       </form>
                   </div>
 
@@ -44,5 +71,6 @@ include "admin/config.php";
   </section>
 
 <?php 
-include "footer.php"; 
+include "footer.php";
+
 ?>
