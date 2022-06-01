a380d365e8c8630055f4d3dc9694eeb8110dff63
Lutfiahsalim <Lutfiah.salim12@gmail.com>
2022-06-01 17:39:59 +0800

    perbaikan halaman admin statistik

--- admin/admin-statistika/edit_penduduk.php
+++ admin/admin-statistika/edit_penduduk.php
@@ -8,16 +8,35 @@ if (isset($_POST['submit'])){
     $penduduk_id = $_POST['penduduk_id'];
     $jenis_data = $_POST['jenis_data'];
     $jumlah = $_POST['jumlah'];
-   
-    $query = "UPDATE penduduk_desa SET jenis_data='$jenis_data',jumlah='$jumlah'
+
+    $old_filename = $_POST['old_image'];
+    $url_gambar = $_FILES['url_gambar']['name'];
+    $update_filename = "";
+    if($url_gambar!=NULL){
+        // rename image
+        $image_extension = pathinfo($url_gambar, PATHINFO_EXTENSION);
+        $filename =time().'.'.$image_extension;
+        $update_filename = $filename;
+    } else{
+        $update_filename= $old_filename;
+    }
+    
+    $query = "UPDATE penduduk SET jenis_data='$jenis_data',jumlah='$jumlah',url_gambar='$update_filename'
                                     WHERE id='$penduduk_id'" ;
 
 	$query_run=mysqli_query($conn, $query);
 
     if($query_run)
-    {        
-        $_SESSION['message'] = "Update berhasil!";
-        header('Location: edit_penduduk.php?id='.$penduduk_id);
+    {
+        if($url_gambar!=NULL){
+            if(file_exists('../uploads/statistik/'.$old_filename)){
+                unlink("../uploads/statistik/'.$old_filename");
+            }
+            move_uploaded_file($_FILES['url_gambar']['tmp_name'], '../uploads/statistik/'.$update_filename);
+        }
+              
+        // $_SESSION['message'] = "Update berhasil!";
+        header('Location: index.php');
         exit(0);
     }
     else
@@ -30,6 +49,8 @@ if (isset($_POST['submit'])){
 
 ?>
 
+?>
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
     <section class="create-admin-potensi">
@@ -39,7 +60,7 @@ if (isset($_POST['submit'])){
         <?php
             if(isset($_GET['id'])){
                 $penduduk_id = $_GET['id'];
-                $penduduk_query = "SELECT * FROM penduduk_desa WHERE id='$penduduk_id' LIMIT 1";
+                $penduduk_query = "SELECT * FROM penduduk WHERE id='$penduduk_id' LIMIT 1";
                 $penduduk_query_res = mysqli_query($conn, $penduduk_query);
 
                 if(mysqli_num_rows($penduduk_query_res)>0){
@@ -49,11 +70,16 @@ if (isset($_POST['submit'])){
                         <input type="hidden" name="penduduk_id" value="<?= $penduduk_row['id']?>">
                         <div class="mb-3">
                             <label for="jenis_data" class="form-label">Jenis Data</label>
-                            <input type="text" class="form-control" id="jenis_data" name="jenis_data" autocomplete="off" required placeholder="Masukkan data" value="<?= $penduduk_row['jenis_data']?>">
+                            <input type="text" class="form-control" id="jenis_data" name="jenis_data" autocomplete="off" required placeholder="Masukkan jenis data" value="<?= $penduduk_row['jenis_data']?>">
                         </div>
                         <div class="mb-3">
-                            <label for="jumlah" class="form-label">jumlah</label>
+                            <label for="jumlah" class="form-label">Jumlah</label>
                             <input type="text" class="form-control" id="jumlah" name="jumlah" rows="5" autocomplete="off" required placeholder="Masukkan jumlah data" value="<?= $penduduk_row['jumlah']?>">
+                        </div>
+                        <div class="mb-3">
+                            <label for="url_gambar" class="label-foto">Foto</label>
+                            <input type="hidden" name="old_image" value="<?= $penduduk_row['url_gambar']?>"/> 
+                            <input type="file" name="url_gambar" id="image">
                         </div>
                         <button type="submit" class="btn btn-primary" name="submit" value="Upload">Update</button>
                     </form>
